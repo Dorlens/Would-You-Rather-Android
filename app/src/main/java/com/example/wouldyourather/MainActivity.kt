edfd7fbb531e95003277e06febdea6c527b2fd03
Dorlens Janvier <dorlens.j03@gmail.com>
2026-06-22 14:43:38 -0400

    updatedvthe ui to the hompage

--- app/src/main/java/com/example/wouldyourather/MainActivity.kt
+++ app/src/main/java/com/example/wouldyourather/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
@@ -14,7 +16,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -58,7 +63,7 @@ fun GameBackground() {
                 .size(260.dp)
                 .offset(x = 220.dp, y = (-80).dp)
                 .background(
-                    Color(0xFFFF6B35).copy(alpha = 0.1f),
+                    Color(0xFFFF6B35).copy(alpha = 0.2f),
                     CircleShape
                 )
         )
@@ -69,7 +74,7 @@ fun GameBackground() {
                 .size(220.dp)
                 .offset(x = (-80).dp, y = 650.dp)
                 .background(
-                    Color(0xFF4ECDC4).copy(alpha = 0.1f),
+                    Color(0xFF4ECDC4).copy(alpha = 0.2f),
                     CircleShape
                 )
         )
@@ -82,10 +87,33 @@ fun GameBackground() {
                 .rotate(12f)
                 .background(
                     Color(0xFFFFE66D).copy(alpha = 0.15f),
-                    RoundedCornerShape(4.dp)
+                    RectangleShape
                 )
         )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 100.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Text(
+                text = "🤔",
+                fontSize = 40.sp,
+                lineHeight = 52.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(top = 100.dp)
+                    .background(
+                        brush = Brush.linearGradient(
+                            colors = listOf(Color(0xFFFF6B35), Color(0xFFFF3CAC))
+                        ),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .padding(16.dp)
 
+            )
+        }
         //Would you rather text
         Column(
             modifier = Modifier
@@ -140,6 +168,65 @@ fun GameBackground() {
                     .padding(top = 14.dp)
             )
         }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 500.dp),
+            horizontalAlignment = Alignment.Start
+        )
+        {
+            Box(
+                modifier = Modifier
+                    .padding(start = 60.dp)
+                    .size(width = 127.dp, height = 50.dp)
+                    .background(Color(0xFFFF6B35), RoundedCornerShape(12.dp)),
+                contentAlignment = Alignment.Center
+
+            )
+            {
+                Text("Option A", color = Color(0xFF0F0E17), fontSize = 14.sp)
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 500.dp),
+            horizontalAlignment = Alignment.Start
+        )
+        {
+            Box(
+                modifier = Modifier
+                    .padding(start = 200.dp)
+                    .size(50.dp)
+                    .background(Color(0xFFFFE66D), CircleShape),
+                contentAlignment = Alignment.Center
+
+            )
+            {
+                Text("OR", color = Color(0xFF0F0E17), fontSize = 12.sp)
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 500.dp),
+            horizontalAlignment = Alignment.Start
+        )
+        {
+            Box(
+                modifier = Modifier
+                    .padding(start = 268.dp)
+                    .size(width = 120.dp, height = 50.dp)
+                    .background(Color(0xFF4ECDC4), RoundedCornerShape(12.dp)),
+                contentAlignment = Alignment.Center
+
+
+            )
+            {
+                Text("Option B", color = Color(0xFF0F0E17), fontSize = 14.sp)
+            }
+        }
 
 
         //Play Now Button
@@ -152,10 +239,22 @@ fun GameBackground() {
         {
             Button(
                 onClick = {},
+                shape = CircleShape,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White
+                ),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
                     .size(width = 150.dp, height = 50.dp)
+                    .background(
+                        brush = Brush.linearGradient(
+                            colors = listOf(Color(0xFFFF6B35), Color(0xFFFF3CAC))
+                        ),
+                        shape = CircleShape
+                    )
+
 
             )
             {
@@ -173,10 +272,11 @@ fun GameBackground() {
         ) {
             Button(
                 onClick = {},
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp)
-                        .size(width = 150.dp, height = 50.dp)
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White.copy(alpha = 0.1f)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .size(width = 150.dp, height = 50.dp)
 
             ) {
                 Text("How to Play")
